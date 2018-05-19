1db5451d52aad8dac4fecd7f65f96998e7e6d17d
Tanja G <tanja.golinski@web.de>
2018-05-19 20:16:59 +0200

    TestCommit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl = 7;
+            Console.WriteLine("{0}", zahl);
+            Console.ReadLine();
+        }
+    }
+}
